d4e136ffab877c6ea66ccb03ed35d9850f1155d2
OshiWindsor <36620242+OshiWindsor@users.noreply.github.com>
2020-08-17 09:48:10 +0530

    History order details

--- lib/model/order.dart
+++ lib/model/order.dart
@@ -8,12 +8,13 @@ class Order {
       contact,
       nic,
       ordered_at,
-      delivery_charges,
-      full_amount,
       prescription_url;
 
+  final int delivery_charges,
+      full_amount;
+
   Order(
-      {this.id,
+      this.id,
         this.clientId,
         this.name,
         this.delivery_address,
@@ -24,21 +25,21 @@ class Order {
         this.ordered_at,
         this.delivery_charges,
         this.full_amount,
-        this.prescription_url});
+        this.prescription_url);
 
-  factory Order.fromJson(Map<String, dynamic> json) {
-    return Order(
-        id: json['nid'],
-        clientId: json['clientId'],
-        name: json['name'],
-        delivery_address: json['delivery_address'],
-        email: json['email'],
-        patient: json['patient_name'],
-        contact: json['contact_number'],
-        nic: json['nic'],
-        ordered_at: json['ordered_at'],
-        delivery_charges: json['delivery_charges'],
-        full_amount: json['full_amount'],
-        prescription_url: json['prescription_url']);
-  }
+  // factory Order.fromJson(Map<String, dynamic> json) {
+  //   return Order(
+  //       id: json['nid'],
+  //       clientId: json['clientId'],
+  //       name: json['name'],
+  //       delivery_address: json['delivery_address'],
+  //       email: json['email'],
+  //       patient: json['patient_name'],
+  //       contact: json['contact_number'],
+  //       nic: json['nic'],
+  //       ordered_at: json['ordered_at'],
+  //       delivery_charges: json['delivery_charges'],
+  //       full_amount: json['full_amount'],
+  //       prescription_url: json['prescription_url']);
+  // }
 }
