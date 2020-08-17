d21df6ca3aafb45b4dbd154fc44b3bbb38d82959
shanika waidyaratne <shanikawaidyarathne@gmail.com>
2020-08-17 14:35:04 +0530

    update pending order status to ongoing

--- lib/screens/orders/pendingOrders.dart
+++ lib/screens/orders/pendingOrders.dart
@@ -26,7 +26,7 @@ class _PendingOrdersState extends State<PendingOrders> {
   String nic;
   String ordered_at;
   String delivery_charges;
-  String full_amount;
+  int full_amount;
   String prescription_url;
 
   Future getPendingOrder() async {
@@ -41,6 +41,59 @@ class _PendingOrdersState extends State<PendingOrders> {
     //print("--------------------------");
     //print(response.body);
     return jsonDecode(response.body)['data'];
+  }
+
+  updateOrderStatus(String orderid,BuildContext cont) async {
+    await SharedPreferences.getInstance().then((prefs) {
+      token = prefs.getString("token");
+    });
+    var decodedToken = new JWT.parse(token);
+    print(decodedToken.claims['id']);
+    String status = 'ongoing';
+    Response response = await post(
+      "https://e-pharma-server.herokuapp.com/driver/updateOrderStatus/" +
+          orderid,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'status': status,
+      }),
+    );
+
+    //print("--------------------------");
+    //print(response.body);
+    Navigator.of(cont).pop();
+    if (response.statusCode == 202) {
+      print("object");
+      //Navigator.pop(context);
+      
+      return showDialog(
+        context: context ,
+        builder: (context) {
+            return AlertDialog(
+               shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          content: Text("success"),
+        );
+        }
+      
+      );
+    } else {
+    return showDialog(
+        context: context ,
+        builder: (context) {
+            return AlertDialog(
+               shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          content: Text("fail"),
+        );
+        }
+      
+      );
+    }
   }
 
   @override
@@ -87,101 +140,113 @@ class _PendingOrdersState extends State<PendingOrders> {
                     );
                   });
             }
-          }), 
+          }),
     );
   }
 
   _popupDialog(orderData) {
     return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-  
-                print('inside showdialog');
+        context: context,
+        builder: (BuildContext context) {
+          print('inside showdialog');
 
-                name = orderData['name'];
-                delivery_address = orderData['delivery_address'];
-                contact = orderData['contact'];
-                nic = orderData['nic'];
-                full_amount = orderData['full_amount'];
-                
-                return AlertDialog(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20.0),
+          name = orderData['name'];
+          delivery_address = orderData['delivery_address'];
+          contact = orderData['contact'];
+          nic = orderData['nic'];
+          full_amount = orderData['full_amount'];
+          String order_id = orderData["_id"];
+
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            title: Center(child: Text("Order Details")),
+            content: Column(
+              children: <Widget>[
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.person),
+                      labelText: "Name",
+                    ),
+                    initialValue: name,
+                    enabled: false,
                   ),
-                  title: Center(child: Text("Order Details")),
-                  content: Column(
-                    children: <Widget>[
-                      Container(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            icon: const Icon(Icons.person),
-                            labelText: "Name",
-                          ),
-                          initialValue: name,
-                          enabled: false,
-                        ),
-                      ),
-                      Container(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            icon: const Icon(Icons.room),
-                            labelText: "Delivery Address",
-                          ),
-                          initialValue: delivery_address,
-                          enabled: false,
-                        ),
-                      ),
-                      Container(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            icon: const Icon(Icons.phone),
-                            labelText: "Contact",
-                          ),
-                          initialValue: contact,
-                          enabled: false,
-                        ),
-                      ),
-                      Container(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            icon: const Icon(Icons.chrome_reader_mode),
-                            labelText: "NIC",
-                          ),
-                          initialValue: nic,
-                          enabled: false,
-                        ),
-                      ),
-                      Container(
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            icon: const Icon(Icons.attach_money),
-                            labelText: "Full Amount",
-                          ),
-                          initialValue: full_amount,
-                          enabled: false,
-                        ),
-                      ),
-                    ],
+                ),
+                Container(
+                  child: TextFormField(
+                    //expands: true,
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.room),
+                      labelText: "Delivery Address",
+                    ),
+                    initialValue: delivery_address,
+                    enabled: false,
                   ),
-                  
-                  actions: <Widget>[
-                    Center(
-                      child: FlatButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        color: Colors.cyan[400],
-                        onPressed: (){}, 
-                        splashColor: Colors.blueGrey,
-                        child: Text(
-                          'OK',
-                          style: TextStyle(color:Colors.white, fontSize: 16.0),
-                          ),
+                ),
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.phone),
+                      labelText: "Contact",
+                    ),
+                    initialValue: contact,
+                    enabled: false,
+                  ),
+                ),
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.chrome_reader_mode),
+                      labelText: "NIC",
+                    ),
+                    initialValue: nic,
+                    enabled: false,
+                  ),
+                ),
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.attach_money),
+                      labelText: "Full Amount",
+                    ),
+                    initialValue: full_amount.toString(),
+                    enabled: false,
+                  ),
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              Row(
+                children: <Widget>[
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 8,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    child: FlatButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
                       ),
-                    )
-                  ],
-                );
-              
-      });
+                      color: Colors.cyan[400],
+                      onPressed: () {
+                        updateOrderStatus(order_id,context);
+                      },
+                      splashColor: Colors.blueGrey,
+                      child: Text(
+                        'ACCEPT',
+                        style: TextStyle(color: Colors.white, fontSize: 16.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 8,
+              )
+            ],
+          );
+        });
   }
 }
