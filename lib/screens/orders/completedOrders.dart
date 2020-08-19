a00fc51d1fb07cf986aeeaaff6ad6dd447b3680d
OshiWindsor <36620242+OshiWindsor@users.noreply.github.com>
2020-08-19 15:54:17 +0530

    Updated completedOrders

--- lib/screens/orders/completedOrders.dart
+++ lib/screens/orders/completedOrders.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'dart:async';
-import 'package:epharma/model/order.dart';
-//import 'package:epharma/services/order_service.dart';
 import 'dart:convert';
+import 'package:corsac_jwt/corsac_jwt.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CompletedOrders extends StatefulWidget {
   @override
@@ -12,117 +12,193 @@ class CompletedOrders extends StatefulWidget {
 }
 
 class _CompletedOrdersState extends State<CompletedOrders> {
-  List<Order> orders = [];
-  Future<void> _getOrderData() async {
-    var orderData = await http.get(
-        "https://e-pharma-server.herokuapp.com/admin/orders?value=completed");
-    var jsonData = json.decode(orderData.body);
-    // print(jsonData);
-    List<Order> temp = [];
+  String token;
+  List completedOrderData;
 
-    for (var ord in jsonData['data']) {
-      print("here");
-      print(ord);
+  String id;
+  String name;
+  String clientId;
+  // ignore: non_constant_identifier_names
+  String delivery_address;
+  String email;
+  String patient;
+  String contact;
+  String nic;
+  // ignore: non_constant_identifier_names
+  String ordered_at;
+  // ignore: non_constant_identifier_names
+  String delivery_charges;
+  // ignore: non_constant_identifier_names
+  int full_amount;
+  // ignore: non_constant_identifier_names
+  String prescription_url;
 
-      //print(ord);
-      // Order order = new Order(
-      //     ord["name"],
-      //     ord["contact"],
-      //     ord["delivery_address"],
-      //     ord["prescription_url"]
-      //     );
-
-      Order order = new Order(
-          ord["id"],
-          ord["clientId"],
-          ord["name"],
-          ord["delivery_address"],
-          ord["email"],
-          ord["patient"],
-          ord["contact"],
-          ord["nic"],
-          ord["ordered_at"],
-          ord["delivery_charges"],
-          ord["full_amount"],
-          ord["prescription_url"]
-          );
-
-      // Order order = new Order(
-      //     id: ord['id'],
-      //     clientId: ord['clientId'],
-      //     name: ord['name'],
-      //     delivery_address: ord['delivery_address'],
-      //     email: ord['email'],
-      //     patient: ord['patient'],
-      //     contact: ord['contact'],
-      //     nic: ord['nic'],
-      //     ordered_at: ord['ordered_at'],
-      //     delivery_charges: ord['delivery_charges'],
-      //     full_amount: ord['full_amount'],
-      //     prescription_url: ord['prescription_url']);
-      temp.add(order);
-    }
-    setState(() {
-      orders = temp;
+  Future getPendingOrder() async {
+    await SharedPreferences.getInstance().then((prefs) {
+      token = prefs.getString("token");
     });
-    print(orders);
-    // return orders;
-  }
+    var decodedToken = new JWT.parse(token);
+    print(decodedToken.claims['id']);
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _getOrderData();
+    Response response = await get(
+        "https://e-pharma-server.herokuapp.com/driver/completedOrders");
+    return jsonDecode(response.body)['data'];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          child: orders.length == 0
-              ? SpinKitChasingDots(
-                  color: Colors.cyan[400],
-                )
-              : ListView.builder(
-                  itemCount: orders.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage:
-                            NetworkImage(orders[index].prescription_url),
+      body: FutureBuilder(
+          future: this.getPendingOrder(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              print("Loading completed order data ..");
+
+              return Center(
+                child: SpinKitChasingDots(color: Colors.cyan[400]),
+              );
+            } else {
+              print("Order details came to the front end");
+              completedOrderData = snapshot.data;
+
+              return ListView.builder(
+                  itemCount: completedOrderData.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    name = completedOrderData[index]['name'];
+                    contact = completedOrderData[index]['contact'];
+                    return Container(
+                      child: ListTile(
+                        title: Text(completedOrderData[index]['name']),
+                        subtitle:
+                            Text(completedOrderData[index]['delivery_address']),
+                        trailing: Icon(Icons.keyboard_arrow_right),
+                        onTap: () {
+                          _popupDialog(completedOrderData[index]);
+                        },
                       ),
-                      title: Text(orders[index].name.toString()),
-                      subtitle: Text(orders[index].delivery_address.toString()),
-                      trailing: Text(orders[index].contact.toString()),
-                      onTap: () {},
                     );
-                  })
-//          FutureBuilder(
-//           future: _getOrderData(),
-//           builder: (BuildContext context, AsyncSnapshot snapshot) {
-//             if (snapshot.data== null){
-//               return Container(
-//                 child: Center(
-//                   child: Text("Loading...."),
-//                 ),
-//               );
-//             }
-//             else{
-//               return ListView.builder(
-//                 itemCount: snapshot.data.length,
-//                 itemBuilder: (BuildContext context, int index) {
-//                   return ListTile(
-//                     title: Text (snapshot.data[index].name.toString()),
-//                   //  title: Text (snapshot.data[index].title),
-// //                    subtitle: Text(snapshot.data[index].title),
-//                   );
-//                 },
-//               );
-//             }
-//           },
-//         ),
-          ),
+                  });
+            }
+          }),
     );
+  }
+
+  _popupDialog(completedOrderData) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          name = completedOrderData['name'];
+          delivery_address = completedOrderData['delivery_address'];
+          contact = completedOrderData['contact'];
+          nic = completedOrderData['nic'];
+          full_amount = completedOrderData['full_amount'];
+          delivery_charges = completedOrderData['delivery_charges'];
+          prescription_url = completedOrderData['prescription_url'];
+
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            title: Center(child: Text("Order Details")),
+            content: Column(
+              children: <Widget>[
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.person),
+                      labelText: "Name",
+                    ),
+                    initialValue: name,
+                    enabled: false,
+                  ),
+                ),
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.room),
+                      labelText: "Delivery Address",
+                    ),
+                    initialValue: delivery_address,
+                    enabled: false,
+                  ),
+                ),
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.phone),
+                      labelText: "Contact",
+                    ),
+                    initialValue: contact,
+                    enabled: false,
+                  ),
+                ),
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.chrome_reader_mode),
+                      labelText: "NIC",
+                    ),
+                    initialValue: nic,
+                    enabled: false,
+                  ),
+                ),
+                Flexible(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Flexible(
+                        child: TextFormField(
+                          style: TextStyle(
+                            fontSize: 12.0,
+                          ),
+                          decoration: const InputDecoration(
+                            icon: const Icon(Icons.attach_money, size: 12.0,),
+                            labelText: "Full Amount",
+                          ),
+                          initialValue: full_amount.toString(),
+                          enabled: false,
+                        ),
+                      ),
+                      Flexible(
+                        child: TextFormField(
+                          style: TextStyle(
+                            fontSize: 10.0,
+                          ),
+                          decoration: const InputDecoration(
+                            icon: const Icon(Icons.attach_money, size: 12.0,),
+                            labelText: "Delivery Charges",
+                          ),
+                          initialValue: delivery_charges.toString(),
+                          enabled: false,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Flexible(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        //  borderRadius: BorderRadius.circular(5),
+                        image: DecorationImage(
+                            image: new NetworkImage(prescription_url),
+                            fit: BoxFit.fill)),
+                  ),
+                ),
+                Container(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      icon: const Icon(Icons.access_time),
+                      labelText: "Delivered at",
+                    ),
+                    initialValue: "",
+                    enabled: false,
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
